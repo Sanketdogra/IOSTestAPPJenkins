8546797c7eafd46c2763a7770633b1f82d682e99
sanketdogra <sanketdogra@gmail.com>
2017-09-01 14:02:31 -0400

    initial commit

--- TestJenkinsForIOSTests/TestJenkinsForIOSTests.swift
+++ TestJenkinsForIOSTests/TestJenkinsForIOSTests.swift
@@ -24,6 +24,7 @@ class TestJenkinsForIOSTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(5 > 4)
     }
     
     func testPerformanceExample() {
